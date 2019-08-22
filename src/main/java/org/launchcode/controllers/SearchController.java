9172c34b633dde66cb3025bada90f4c7e7f7eb2c
Shandy Casteel <shandycasteel@gmail.com>
2019-08-22 12:50:27 -0500

    Add TechJobsController to extend other controllers and refactor page navigation

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -17,7 +17,7 @@ import static org.launchcode.controllers.ListController.columnChoices;
  */
 @Controller
 @RequestMapping("search")
-public class SearchController {
+public class SearchController extends TechJobsController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
